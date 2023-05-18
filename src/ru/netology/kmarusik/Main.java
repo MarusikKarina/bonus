719dd0c876a49d04b23291480c79ffa414de1527
Karina Marusik <kamarusik@sberbank.ru>
2023-05-18 15:23:04 +0300

    rename variables

--- src/ru/netology/kmarusik/Main.java
+++ src/ru/netology/kmarusik/Main.java
@@ -3,19 +3,19 @@ package ru.netology.kmarusik;
 public class Main {
 
     public static void main(String[] args) {
-        int счет = 100;
-        int пополнение = 1100;
-        int подарокЗаКаждые = 100;
-        int минимальнаяСуммаДляПолученияБонуса = 1000;
+        int account = 100;
+        int payment = 1100;
+        int bonus = 100;
+        int threshold = 1000;
 
-        if (пополнение > минимальнаяСуммаДляПолученияБонуса) {
-            int подарок = пополнение / подарокЗаКаждые;
-            System.out.println("подарок: " + подарок);
-            счет = счет + пополнение + подарок;
+        if (payment > threshold) {
+            int calculatedBonus = payment / bonus;
+            System.out.println("present: " + calculatedBonus);
+            account = account + payment + calculatedBonus;
         } else {
-            счет = счет + пополнение;
+            account = account + payment;
         }
 
-        System.out.println(счет);
+        System.out.println(account);
     }
 }
